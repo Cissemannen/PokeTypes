02190c74f45c7f6a2d4a77db5ce6d25bfe36f9d2
groupinfra\hallci <christer.hall@cgi.com>
2024-07-08 12:59:31 +0200

    Add project files.

---
+++ PokeTypes/PokemonType.cs
@@ -0,0 +1,27 @@
+﻿namespace PokeTypes
+{
+    public class PokemonType
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public double VsNormal { get; set; }
+        public double VsFighting { get; set; }
+        public double VsFlying { get; set; }
+        public double VsPoison { get; set; }
+        public double VsGround { get; set; }
+        public double VsRock { get; set; }
+        public double VsBug { get; set; }
+        public double VsGhost { get; set; }
+        public double VsSteel { get; set; }
+        public double VsFire { get; set; }
+        public double VsWater { get; set; }
+        public double VsGrass { get; set; }
+        public double VsElectric { get; set; }
+        public double VsPsychic { get; set; }
+        public double VsIce { get; set; }
+        public double VsDragon { get; set; }
+        public double VsDark { get; set; }
+        public double VsFairy { get; set; }
+
+    }
+}
